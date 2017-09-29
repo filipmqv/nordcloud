af94bb7b4788db7ed0aa34a7d22d5eaf1e598e70
Filip Waligórski <filip.waligorski@student.put.poznan.pl>
2017-09-29 19:34:43 +0200

    id as parameter to program, updated manager loop

--- script.py
+++ script.py
@@ -4,23 +4,43 @@ import subprocess
 import asyncio
 import datetime
 import sys
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Watchdog')
+    parser.add_argument('id', type=int, help='an integer for the id')
+    return parser.parse_args()
 
 
 async def manage_service(name, num_of_sec_check, num_of_sec_wait, num_of_attempts):
-    while True:
-        print('check ' + name)
+    running = True
+    while running:
+        print(name+ ' check ')
         if not is_service_running(name):
-            print('starting ' + name)
-            start_service(name)
-        print('sleeping ' + name)
-        await asyncio.sleep(5)
+            for i in range(num_of_attempts):
+                print(name + ' starting attempt ' + str(i))
+                start_service(name)
+                if is_service_running(name):
+                    print(name + ' breaking ')
+                    break;
+                if i == num_of_attempts - 1:
+                    running = False
+                    print(name + ' quitting')
+                if running: 
+                    print(name + ' did not start, waiting; times:' + str(i))
+                    await asyncio.sleep(num_of_sec_wait)
+        if running: 
+            print(name + ' sleeping')
+            await asyncio.sleep(num_of_sec_check)
+    print(name + " quited")
 
 
-def get_dynamodb_settings(table_name='fwaligorski-watchdog-table'):
+def get_dynamodb_settings(id, table_name='fwaligorski-watchdog-table'):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(table_name)
+    # TODO check if id is correct
     response = table.get_item(
-        Key={'id': '1'}
+        Key={'id': str(id)}
         )
     items = response['Item']
     print(items['ListOfServices'])
@@ -40,10 +60,14 @@ def is_service_running(name):
 
 
 try:
-    settings = get_dynamodb_settings()
+    args = parse_args()
+    settings = get_dynamodb_settings(args.id)
     # TODO error check!!!
+    # TODO check if settings changed every 15*60 sec
 
-    futures = [manage_service(service, 1, 1, 1) for service in settings['ListOfServices']]
+    futures = [manage_service(service, 
+        int(settings['NumOfSecCheck']), int(settings['NumOfSecWait']), int(settings['NumOfAttempts'])) 
+        for service in settings['ListOfServices']]
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(asyncio.wait(futures))
