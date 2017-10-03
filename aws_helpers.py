fdd681a5877f5ea70b707da91370a2b73e7a4a75
Filip Waligórski <filip.waligorski@student.put.poznan.pl>
2017-10-03 13:42:21 +0200

    removed todo

--- aws_helpers.py
+++ aws_helpers.py
@@ -12,7 +12,6 @@ def sns_send(message, topic_arn='arn:aws:sns:us-west-2:632826021673:fwaligorski-
 def get_dynamodb_settings(id, table_name='fwaligorski-watchdog-table'):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(table_name)
-    # TODO check if id is correct
     response = table.get_item(
         Key={'id': str(id)}
     )
